68662e0b8a48b840845abe60610bebd5ad4e1f15
Alexey <lmokhareva@gmail.com>
2024-02-04 14:33:16 +0300

    Task 4 [React] - Fixed issues

--- src/containers/LoginContainer.tsx
+++ src/containers/LoginContainer.tsx
@@ -1,104 +1,95 @@
-import React, { useState } from "react";
-import Login from '../views/Login/index';
-
-//Определяем тип данных для data
-interface IData {
-    email:string,
-    password:string
-  }
+import React, { useCallback, useState } from "react";
+import Login from "../views/Login/index";
+import { ILoginData } from "../types/types";
 
 //функциональный компонент для стандартного логина
-const LoginContainer : React.FC = () =>{
+const LoginContainer: React.FC = () => {
+  //хуки для отслеживания состояний
+  //Отслеживает ошибку для пароля
+  const [passError, setPassError] = useState("");
 
-    //хуки для отслеживания состояний
-    //Отслеживает ошибку для пароля
-    const [passError, setPassError] = useState('');
+  //Отслеживает ошибку для почты
+  const [emailError, setEmailError] = useState("");
 
-    //Отслеживает ошибку для почты
-    const [emailError, setEmailError] = useState('');
+  //Хранит информацию о текущем пароле и почте
+  const [data, setData] = useState<ILoginData>({ email: "", password: "" });
 
-    //Хранит информацию о текущем пароле и почте
-    const [data,setData] = useState<IData>({ email: '', password: '' });
+  //Отслеживает уведомление
+  const [notification, setNotification] = useState("");
 
-    //Отслеживает уведомление
-    const [notification, setNotification] = useState('');
+  //функция для изменения поля почты
+  const onEmailChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      //Определим регулярное выражение
+      let regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
 
-    //функция для изменения поля почты
-    const onEmailChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
-        //Определим регулярное выражение
-        let regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+      //Получаем почту
+      let email = event.target.value;
 
-        //Получаем почту
-        let email = event.target.value;
+      //Используем хук для изменения почты
+      setData((prevData) => {
+        return { ...prevData, email: email };
+      });
 
-        //Используем хук для изменения почты
-        setData((prevData) => {
-          return { ...prevData, email: email };
-        });
-
-        //Используем хук для проверки почты по регулярному выражению
-        if (!email.match(regex)) {
-          setEmailError('Wrong email format.');
-        }
-        else{
-          setEmailError('');
-        }
-  }
+      //Используем хук для проверки почты по регулярному выражению
+      if (!email.match(regex)) {
+        setEmailError("Wrong email format.");
+      } else {
+        setEmailError("");
+      }
+    },
+    [data]
+  );
 
   //Функция для изменения поля пароля
-  const onPasswordFieldChange = (event: React.ChangeEvent<HTMLInputElement>)=>{
-    let password = event.target.value;
-    setData((prevData) => {
-      return { ...prevData, password: password };
-    });
+  const onPasswordFieldChange = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      let password = event.target.value;
+      setData((prevData) => {
+        return { ...prevData, password: password };
+      });
 
-    //Валидация 6 символов
-    if (password.length <= 6) {
-      setPassError('Less, then 6 characters.')
-    }
-    else{
-      setPassError('');
-    }
-  }
+      //Валидация 6 символов
+      if (password.length <= 6) {
+        setPassError("Less, then 6 characters.");
+      } else {
+        setPassError("");
+      }
+    },
+    [data]
+  );
 
   //Обработчик нажатия на кнопку Enter
-  const onButtonClick = ()=>{
-    //Проверям, введены ли корректные данные
+  const onFormSubmit = useCallback(() => {
     if (!passError && !emailError && data.email && data.password) {
-        //выводим их в консоль
-        console.log('Email: ', data.email);
-        console.log('Password: ', data.password);
-        
-        //Сбрасываем значения
-        setData({ email: '', password: '' });
+      //выводим их в консоль
+      console.log("Email: ", data.email);
+      console.log("Password: ", data.password);
 
-        //Выводим уведомление, что удачно вошли
-        setNotification('Your account has been succesfuly created!');
+      //Сбрасываем значения
+      setData({ email: "", password: "" });
 
-        //Удаляем уведомление через 3 секунды
-        setTimeout(()=>{
-          setNotification('');
-        },3000)
+      //Выводим уведомление, что удачно вошли
+      setNotification("Your account has been successfuly created!");
+
+      //Удаляем уведомление через 3 секунды
+      setTimeout(() => {
+        setNotification("");
+      }, 3000);
     }
-  }
+  }, [data, setData]);
 
-  //Проверка нажатия на клавишу Enter
-  const onEnterPress = (event: React.KeyboardEvent<HTMLInputElement>)=>{
-    if (event.key === "Enter") {
-      onButtonClick();
-    }
-  }
-    return (
-        <Login notification={notification}
-        emailError={emailError}
-        passError={passError}
-        data={data}
-        onEmailChange={onEmailChange}
-        onEnterPress={onEnterPress}
-        onPasswordFieldChange={onPasswordFieldChange}
-        onButtonClick={onButtonClick}
-        />
-    )
-}
+  return (
+    <Login
+      notification={notification}
+      emailError={emailError}
+      passError={passError}
+      data={data}
+      onEmailChange={onEmailChange}
+      onPasswordFieldChange={onPasswordFieldChange}
+      onFormSubmit={onFormSubmit}
+    />
+  );
+};
 
 export default LoginContainer;
--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,30 +1,38 @@
 import React, { useState, ChangeEvent, KeyboardEvent } from "react";
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Alert from '@mui/material/Alert';
-import './styles.css'
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import "./styles.css";
+import { ILoginData } from "../../types/types";
 
-interface IData {
-  email: string;
-  password: string;
-}
-
-interface InformationProps {
+interface LoginProps {
   notification: string;
   emailError: string;
   passError: string;
-  data: IData;
+  data: ILoginData;
   onEmailChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  onEnterPress: (event: KeyboardEvent<HTMLInputElement>) => void;
   onPasswordFieldChange: (event: ChangeEvent<HTMLInputElement>) => void;
-  onButtonClick: () => void;
+  onFormSubmit: () => void;
 }
 
-const Login: React.FC<InformationProps> = (props) => {
+const Login: React.FC<LoginProps> = (props) => {
+  //Флажок для нажатия на кнопку enter
+  const [isEnterInputEnable, setIsEnterInputEnable] = useState(true);
+
+  //Проверка нажатия на клавишу Enter
+  const onEnterPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && isEnterInputEnable) {
+      props.onFormSubmit();
+    }
+  };
+
+  const changeEnterSubmit = () => {
+    setIsEnterInputEnable((prev) => !prev);
+  };
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', color: 'white', padding: '20px', margin: '100px' }}>
+    <div className="login-container">
       {props.notification && (
-        <div style={{ position: 'fixed', top: 20, right: 20, zIndex: 9999 }}>
+        <div className="notification-container">
           <Alert variant="filled" severity="success">
             {props.notification}
           </Alert>
@@ -35,50 +43,57 @@ const Login: React.FC<InformationProps> = (props) => {
         label="Login"
         variant="outlined"
         inputProps={{
-          style: { color: 'white', border: '1px solid white', borderRadius: '4px' },
+          style: {
+            color: "white",
+            border: "1px solid white",
+            borderRadius: "4px",
+          },
         }}
         InputLabelProps={{
-          style: { color: 'white', background: 'black' },
+          style: { color: "white", background: "black" },
         }}
         placeholder="Enter your login"
-        style={{ margin: '10px' }}
+        style={{ margin: "10px" }}
         onChange={props.onEmailChange}
         error={Boolean(props.emailError)}
         helperText={props.emailError}
         value={props.data.email}
-        onKeyDown={props.onEnterPress}
+        onKeyDown={onEnterPress}
       />
       <TextField
         id="outlined-basic"
         label="Password"
         variant="outlined"
         inputProps={{
-          style: { color: 'white', border: '1px solid white', borderRadius: '4px' },
+          style: {
+            color: "white",
+            border: "1px solid white",
+            borderRadius: "4px",
+          },
         }}
         InputLabelProps={{
-          style: { color: 'white', background: 'black' },
+          style: { color: "white", background: "black" },
         }}
         placeholder="Enter your password"
-        style={{ margin: '10px' }}
+        style={{ margin: "10px" }}
         onChange={props.onPasswordFieldChange}
         error={Boolean(props.passError)}
         helperText={props.passError}
         type="password"
         value={props.data.password}
-        onKeyDown={props.onEnterPress}
+        onKeyDown={onEnterPress}
       />
       <Button
         variant="outlined"
-        sx={{ fontSize: '1.15rem' }}
-        style={{ margin: '10px' }}
-        onClick={props.onButtonClick}>
+        sx={{ fontSize: "1.15rem" }}
+        style={{ margin: "10px" }}
+        onClick={props.onFormSubmit}
+      >
         Enter
       </Button>
-      <div className="counter-block" style={{ marginTop: '20px', fontSize: '18px', color: 'white', margin: '70px' }}>
-        <div style={{ color: 'white', fontSize: '16px', margin: '10px' }}>
-          Current email: {props.data.email}
-        </div>
-        <div style={{ color: 'white', fontSize: '16px', margin: '10px' }}>
+      <div className="counter-block">
+        <div className="email-info">Current email: {props.data.email}</div>
+        <div className="password-info">
           Current password: {props.data.password}
         </div>
       </div>
