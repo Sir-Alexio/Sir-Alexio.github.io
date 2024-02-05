0964715ff00c3c9db05d2b417b2c57f21fed29da
Alexey <lmokhareva@gmail.com>
2024-02-05 18:59:24 +0300

    Revert "Added json files"
    This reverts commit 94a51627bdaa5cfd827691c42d33a6b78742b5d7.

--- src/redux/Saga/saga.tsx
+++ src/redux/Saga/saga.tsx
@@ -1,33 +1,24 @@
-import { takeLatest, call, put } from "redux-saga/effects";
-import axios, { AxiosError } from "axios";
+import { takeLatest, call, put } from 'redux-saga/effects';
+import axios, { AxiosError } from 'axios';
 
-interface IDataActivity {
-  data: {
-    activity: string;
-  };
+interface IDataActivity{
+    data:{
+        activity:string;
+    }
 }
 
 function* fetchDataSaga() {
   try {
-    const response: IDataActivity = yield call(
-      axios.get,
-      "https://www.boredapi.com/api/activity/"
-    );
-
-    yield put({
-      type: "FETCH_TASKS_SUCCESS",
-      payload: { activity: response.data.activity },
-    });
-  } catch (apiError: any) {
-    console.log("Api error: ", apiError);
-
-    yield put({
-      type: "FETCH_TASKS_FAILURE",
-      payload: { error: apiError.message },
-    });
+    const response:IDataActivity = yield call(axios.get, 'https://www.boredapi.com/api/activity/');
+    
+    yield put({ type: 'FETCH_TASKS_SUCCESS', payload:{activity:response.data.activity}  });
+  } catch (apiError:any) {
+    console.log('Api error: ',apiError);
+    
+    yield put({ type: 'FETCH_TASKS_FAILURE', payload:{error: apiError.message} });
   }
 }
 
 export function* watchFetchData() {
-  yield takeLatest("FETCH_DATA", fetchDataSaga);
+  yield takeLatest('FETCH_DATA', fetchDataSaga);
 }
