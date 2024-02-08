59c13b83b555981dbcf02cd21c402b0cd54d93b5
Alexey <lmokhareva@gmail.com>
2024-02-08 15:56:03 +0300

    tast 1 [ts] - removed interface folder

--- src/containers/CounterContainer.tsx
+++ src/containers/CounterContainer.tsx
@@ -1,37 +1,39 @@
+import React from "react";
+import Counter from "../views/index";
 
-import React from 'react';
-import Counter from '../views/index';
-import {ICounterContainer, StateType} from '../Interfaces/index';
+class CounterContainer extends React.Component {
+  state = {
+    currentCounter: 0,
+  };
 
-class CounterContainer extends React.Component<ICounterContainer,StateType>{
-    state = {
-        currentCounter: 0
-    }
+  onIncrementClick = () => {
+    this.setState({
+      currentCounter: this.state.currentCounter + 1,
+    });
+  };
 
-    onIncrementClick = ()=>{
-        this.setState({
-            currentCounter: this.state.currentCounter+1
-        });
-    }
+  onDecrementClick = () => {
+    this.setState({
+      currentCounter: this.state.currentCounter - 1,
+    });
+  };
 
-    onDecrementClick = ()=>{
-        this.setState({
-            currentCounter: this.state.currentCounter-1
-        });
-    }
+  onResetClick = () => {
+    this.setState({
+      currentCounter: 0,
+    });
+  };
 
-    onResetClick = ()=>{
-        this.setState({
-            currentCounter: 0
-        });
-    }
-
-    render(){
-        return <Counter state ={this.state}
-                onIncrementClick={this.onIncrementClick} 
-                onDecrementClick={this.onDecrementClick} 
-                onResetClick={this.onResetClick} />
-    }
+  render() {
+    return (
+      <Counter
+        currentCounter={this.state.currentCounter}
+        onIncrementClick={this.onIncrementClick}
+        onDecrementClick={this.onDecrementClick}
+        onResetClick={this.onResetClick}
+      />
+    );
+  }
 }
 
 export default CounterContainer;
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import Styles from './styles';
-import Button from '@mui/material/Button';
-import {ICounterContainer} from '../Interfaces/index';
+import React from "react";
+import Styles from "./styles";
+import Button from "@mui/material/Button";
 
-  const Counter: React.FC<ICounterContainer> = (props) => {
-    const {state,onIncrementClick,onDecrementClick,onResetClick} = props;
-    return (
+interface ICounterContainer {
+  currentCounter: number;
+  onIncrementClick: () => void;
+  onDecrementClick: () => void;
+  onResetClick: () => void;
+}
+
+const Counter: React.FC<ICounterContainer> = (props) => {
+  const { currentCounter, onIncrementClick, onDecrementClick, onResetClick } =
+    props;
+  return (
     <div>
-        <div style={Styles.counterStyle}>Current count: {state.currentCounter}</div>
-        <Button style={Styles.buttonStyle} onClick={onIncrementClick} variant="contained" color="primary">
-            Increment
-        </Button>
-        <Button style={Styles.buttonStyle} onClick={onDecrementClick} variant="contained" color="primary">
-            Decrement
-        </Button>
-        <Button style={Styles.buttonStyle} onClick={onResetClick} variant="contained" color="primary">
-            Reset
-        </Button>
+      <div style={Styles.counterStyle}>Current count: {currentCounter}</div>
+      <Button
+        style={Styles.buttonStyle}
+        onClick={onIncrementClick}
+        variant="contained"
+        color="primary"
+      >
+        Increment
+      </Button>
+      <Button
+        style={Styles.buttonStyle}
+        onClick={onDecrementClick}
+        variant="contained"
+        color="primary"
+      >
+        Decrement
+      </Button>
+      <Button
+        style={Styles.buttonStyle}
+        onClick={onResetClick}
+        variant="contained"
+        color="primary"
+      >
+        Reset
+      </Button>
     </div>
-    )
+  );
 };
 
 export default Counter;
